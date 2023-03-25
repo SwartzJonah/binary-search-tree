b5c5ba55e9e28232845858d1c2a117e568209c7b
SwartzJonah <swartzjonah@gmail.com>
2023-03-25 15:49:38 -0400

    node class and start of tree

---
+++ binary-tree.js
@@ -0,0 +1,14 @@
+function Node(value = null, left= null, right=null) {
+    return { value, leftChild, rightChild };
+}
+
+function Tree(array){
+    this.root = this.buildTree(array);
+
+    function buildTree(array){
+        if(array.length === 0){
+            return []
+        }
+    }
+}
+
