451d2368d158476edeec73d13dba35a46fe3975a
SwartzJonah <swartzjonah@gmail.com>
2023-04-21 17:42:08 -0400

    added buildtree and pretty print funcitons

--- binary-tree.js
+++ binary-tree.js
@@ -1,23 +1,42 @@
-function Node(value = null, left= null, right=null) {
-    return { value, leftChild, rightChild };
+function Node(value = null, left = null, right = null) {
+    return { value, left, right };
 }
 
-function Tree(array){
+function Tree(array) {
     this.root = buildTree(array);
 
-    function buildTree(array){
-        if(array.length === 0){
+    function buildTree(array) {
+        if (array.length === 0) {
             return;
         }
-        let node = new Node(array[0])
-        let temp = new Node();
-        for(i = 1 i < array.length; i++){
-            if (array[i] < node.value){
-                while(node.left === null){
+        let sortedArray = [...new Set(array)].sort((a, b) => a - b);
+        let mid = parseInt(sortedArray.length / 2);
+        const root = new Node(sortedArray[mid], buildTree(sortedArray.slice(0, mid)), buildTree(sortedArray.slice(mid + 1)));
+        return root;
+    }
 
-                }
-            }
+    const prettyPrint = (node, prefix = '', isLeft = true) => {
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+        if (node.left !== null) {
+            prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
         }
     }
+    console.log(this.root);
+    prettyPrint(this.root);
+    
+ 
 }
+
+
+
+//testing area
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+
 
